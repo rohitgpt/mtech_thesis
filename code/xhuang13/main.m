f71ecfe55c40a26a17ea74e5c9c730147f3b74e9
Rohit Gupta <rohitd@iitk.ac.in>
2019-05-02 16:38:04 +0530

    Added files

--- code/xhuang13/main.m
+++ code/xhuang13/main.m
@@ -1,12 +1,12 @@
 function main(L, H, Vf, nx, ny)
 
-delta = 0.02; %change in volume in every iteration
-rmin = 4;     %for mesh independent filter
+delta = 0.1; %change in volume in every iteration
+rmin = 3;     %for mesh independent filter
 
-x = random_init(nx, ny, Vf);   %initial design for microscale FEM
-% xinit = design1(nx, ny);
-% xinit = design2(nx, ny);
-
+% x = random_init(nx, ny, Vf);   %initial design for microscale FEM
+x = design1(nx, ny);
+% x = design2(nx, ny);
+E = [1 0.01];
 syms xa ya;
 alpha = 25e-6;
 beta = 25e-6;
@@ -31,43 +31,45 @@ subs(subs(B, xa, alpha*gauss), ya, beta*gauss) + ...
 subs(subs(B, xa, -alpha*gauss), ya, beta*gauss));
 
 prev_C=1e10;
+prev_s = zeros(ny, nx);
 C = 1e5;
-while (prev_C-C)/C>0.05
-plot_fig(x);
-u = microFEM(nx, ny, x, KE, b, Ddash);
-
-Dh = homogenization(nx, ny, b, u, D);
-
-[C, U] = macroFEM(L, H, Dh, B);
-
-s = sensitivity(L, H, Ddash, b, u, B, U);
-
-s_filtered = apply_filter(s, rmin);
-
-avg_s = 0.5*(s_filtered + prev_s);
-prev_s = avg_s;
-
+count=0;
 v = sum(x(:))/(nx*ny);
-if v>Vf
-  if v*(1-delta)<Vf
-    v = Vf;
-  else
-    v = v*(1-delta);
-  end
-else
-  if v*(1+delta)>Vf
-    v = Vf;
-  else
-    v = v*(1+delta);
-  end
-end
+while abs((prev_C-C)/C)>0.05 || v~=Vf
+  prev_C = C;
+  count=count+1;
+  plot_fig(x);
+  u = microFEM(nx, ny, x, KE, b, B, Ddash, E);
 
-[~, index] = sort(avg_s(:), 'descend');
-v = v*(nx*ny);
-y = zeros(nx*ny, 1);
-y(index(1:v))=1;
-x = reshape(y, nx, ny);
+  Dh = homogenization(nx, ny, x, b, u, Ddash, E);
+  [C, U] = macroFEM(L, H, Dh, B);
+%   disp(abs((prev_C-C)/C));
+%   disp(C);
+%   disp(prev_C);
+  s = sensitivity(nx, ny, L, H, Ddash, b, u, B, U);
+  s_filtered = apply_filter(nx, ny, s, rmin);
 
+  avg_s = 0.5*(s_filtered + prev_s);
+  prev_s = avg_s;
+
+  if v>Vf
+    if v*(1-delta)<Vf
+      v = Vf;
+    else
+      v = v*(1-delta);
+    end
+  else
+    if v*(1+delta)>Vf
+      v = Vf;
+    else
+      v = v*(1+delta);
+    end
+  end
+
+  [~, index] = sort(avg_s(:), 'descend');
+  y = zeros(nx*ny, 1);
+  y(index(1:ceil(v*(nx*ny))))=1;
+  x = reshape(y, ny, nx);
 end
 
 end
@@ -89,8 +91,9 @@ for i=1:nx
 end
 end
 
-function s = sensitivity(L, H, D1, b, u, B, U)
+function s = sensitivity(nx, ny, L, H, D1, b, u, B, U)
 D2 = zeros(3,3);
+syms xa ya;
 s(1:ny, 1:nx) = 1;
 for i=1:nx
   for j=1:ny
@@ -99,35 +102,30 @@ for i=1:nx
     n1 = (ny+1)*(i-1)+j;
     n2 = (ny+1)*i+j;
     dof = [2*n1-1; 2*n1; 2*n2-1; 2*n2; 2*n2+1; 2*n2+2; 2*n1+1; 2*n1+2];
-    s(j, i) = double(int(int((eye(3)-B*u(dof,3))'*(D1-D2)*(eye(3)-B*u(dof,3)), xa, -alpha, alpha), ya, -beta, beta));
+    t2 = double(int(int((eye(3)-B*u(dof,:))'*(D1-D2)*(eye(3)-B*u(dof,:)), xa, -alpha, alpha), ya, -beta, beta));
+    alpha = 1;
+    beta = 1;
+    t1 = double(int(int(B'*t2*B, xa, -alpha, alpha), ya, -beta, beta));
     temp = 0;
     for l=1:L
       for h=1:H
-        alpha = 1;
-        beta = 1;
         n1 = (H+1)*(l-1)+h;
         n2 = (H+1)*l+h;
         dof = [2*n1-1; 2*n1; 2*n2-1; 2*n2; 2*n2+1; 2*n2+2; 2*n1+1; 2*n1+2];
-        t1 = double(int(int(B'*s(j,i)*B, xa, -alpha, alpha), ya, -beta, beta));
         temp = temp + U(dof, 1)'*t1*U(dof,1);
       end
     end
     s(j, i) = temp;
   end
 end
-
-for i=1:L
-  for j=1:H
-    
-  end
-end
-
 end
 
 function plot_fig(x)
+disp(x);
 figure(1)
 colormap(gray);
 imagesc(1-x);
+pause(1);
 end
 
 function [C, U] = macroFEM(L, H, Dh, B)
@@ -136,8 +134,8 @@ K = sparse(2*(L+1)*(H+1),2*(L+1)*(H+1));
 F = sparse(2*(L+1)*(H+1), 1);
 alpha = 1;
 beta = 1;
+syms xa ya;
 KE = double(int(int(B'*Dh*B, xa, -alpha, alpha), ya, -beta, beta));
-
 for i=1:L
   for j=1:H
     n1 = (H+1)*(i-1)+j;
@@ -146,70 +144,73 @@ for i=1:L
     K(dof, dof) = K(dof, dof) + KE;
   end
 end
-n2 = H*(L-1)+ceil(H/2);
+n2 = (H+1)*(L)+ceil(H/2);
 F(2*n2+2, 1) = -1;
-fixed_dofs = [1:2*(H+1)];
-free_dofs = setdiff([1:2*(H+1)*(L+1)], fixed_dofs);
+fixed_dofs = 1:2*(H+1);
+free_dofs = setdiff(1:2*(H+1)*(L+1), fixed_dofs);
 U(free_dofs, :) = K(free_dofs, free_dofs)\F(free_dofs, :);
 U(fixed_dofs, :) = 0;
 
-C = 0.5*F'*U;
+C = double(0.5*F'*U);
 end
 
-function Dh = homogenization(nx, ny, b, u, D)
+function Dh = homogenization(nx, ny, x, b, u, D, E)
 Dh =  zeros(3,3); %initialising Dh to be 3x3
 for i=1:nx
   for j=1:ny
     n1 = (ny+1)*(i-1)+j;
     n2 = (ny+1)*i+j;
     dof = [2*n1-1; 2*n1; 2*n2-1; 2*n2; 2*n2+1; 2*n2+2; 2*n1+1; 2*n1+2];
-    Dh = Dh + (eye(3)-b*u(dof, 3));
+    Dh = Dh + (x(j, i)*E(1)+(1-x(j, i))*E(2))*(eye(3)-b*u(dof, :));
   end
 end
-
 Dh = Dh*D/(nx*ny);
 end
 
-function u = microFEM(nx, ny, x, ke, b, D)
+function u = microFEM(nx, ny, x, ke, b, B, D, E)
 le = b'*D;
-load = sparse(2*(nx+1)*(ny+1), 3);
-k = sparse(2*(nx+1)*(ny+1), 2*(nx+1)*(ny+1));
-u = sparse(2*(nx+1)*(ny+1), 3);
+load = zeros(2*(nx+1)*(ny+1),3);
+k = zeros(2*(nx+1)*(ny+1), 2*(nx+1)*(ny+1));
+% load = sparse(2*(nx+1)*(ny+1), 3);
+% k = sparse(2*(nx+1)*(ny+1), 2*(nx+1)*(ny+1));
+% u = sparse(2*(nx+1)*(ny+1), 3);
+% disp(ke);
+% disp(x);
 for i=1:nx
   for j=1:ny
     n1 = (ny+1)*(i-1)+j;
     n2 = (ny+1)*i+j;
     dof = [2*n1-1; 2*n1; 2*n2-1; 2*n2; 2*n2+1; 2*n2+2; 2*n1+1; 2*n1+2];
-    k(dof, dof) = k(dof, dof) + x(j, i)*ke;
-    load(dof, :) = load(dof, :) + x(j, i)*le;
+%     disp(dof);
+    k(dof, dof) = k(dof, dof) + (x(j, i)*E(1)+(1-x(j, i))*E(2))*ke;
+    load(dof, :) = load(dof, :) + (x(j, i)*E(1)+(1-x(j, i))*E(2))*le;
   end
 end
+% disp(k);
+% disp(inv(k));
 u = k\load;
+% disp(u);
 end
 
-function xinit = random_init(nx, ny, Vf)
+function x = random_init(nx, ny, Vf)
 xinit = rand(ny, nx);
-for j=1:ny
-  for i=1:nx
-    if(xinit(j,i)>Vf) 
-      xinit(j,i)=1;
-    else
-      xinit(j,i)=0;
-    end
-  end
-end
+[~, index] = sort(xinit(:), 'descend');
+v = Vf*(nx*ny);
+y = zeros(nx*ny, 1);
+y(index(1:ceil(v)))=1;
+x = reshape(y, ny, nx);
 end
 
 function xinit = design1(nx, ny)
-xinit(1:ny, 1:nx) = 0;
+xinit(1:ny, 1:nx) = 1;
 xmid = ceil(nx/2); ymid=ceil(ny/2);
-xinit(ymid:ymid+1,xmid:xmid+1) = 1;
+xinit(ymid:ymid+1,xmid:xmid+1) = 0;
 end
 
 function xinit = design2(nx, ny)
-xinit(1:ny, 1:nx) = 0;
-xinit(1,1)=1;
-xinit(ny,1)=1;
-xinit(1,nx)=1;
-xinit(ny, nx)=1;
+xinit(1:ny, 1:nx) = 1;
+xinit(1,1)=0;
+xinit(ny,1)=0;
+xinit(1,nx)=0;
+xinit(ny, nx)=0;
 end
--- code/xhuang13/temp.m
+++ code/xhuang13/temp.m
@@ -9,18 +9,19 @@
 % median(median(x))
 % median(x(:))
 
-nx = 4;
-ny = 3;
-x = randi(10, nx, ny)
-[~, index] = sort(x(:), 'descend');
-v=5;
-y = zeros(nx*ny, 1);
-y(index(1:v))=1;
-reshape(y, nx, ny)
-y = x(:);
-y(index(v+1:length(y))) = 0;
-y = reshape(y, nx, ny);
-a = y>0
+% nx = 4;
+% ny = 3;
+% x = randi(10, nx, ny)
+% [~, index] = sort(x(:), 'descend');
+% v=5;
+% y = zeros(nx*ny, 1);
+% y(index(1:v))=1;
+% reshape(y, nx, ny)
+% y = x(:);
+% y(index(v+1:length(y))) = 0;
+% y = reshape(y, nx, ny);
+% a = y>0
+
 % tmp = zeros(size(x));
 % for i=1:length(y)
 %   if mod(i, ny+1)~=0 
@@ -38,3 +39,20 @@ a = y>0
 %     Out{k1} = ColNrs(Idx(RowNrSort == k1));
 % end
 % ot = [Out{1}, Out{2}, Out{3}]
+
+function s_filtered = apply_filter(nx, ny, s, rmin)
+s_filtered = zeros(size(s));
+for i=1:nx
+  for j=1:ny
+    total = 0;
+    for k = max(i-floor(rmin),1):min(i+floor(rmin),nx)
+      for l = max(j-floor(rmin),1):min(j+floor(rmin),ny)
+        weight = rmin-sqrt((i-k)^2+(j-l)^2);
+        s_filtered(j, i) = s(l, k)*weight;
+        total = total + weight;
+      end
+    end
+    s_filtered(j, i) = s_filtered(j, i)/total;
+  end
+end
+end
