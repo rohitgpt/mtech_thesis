07c05f110d037b4b0b805c78d1a665feef4616b1
Rohit Gupta <rohitd@iitk.ac.in>
2019-05-04 02:50:22 +0530

    Added files

--- code/xhuang13/main.m
+++ code/xhuang13/main.m
@@ -1,12 +1,11 @@
 function main(L, H, Vf, nx, ny)
-
 delta = 0.1; %change in volume in every iteration
 rmin = 3;     %for mesh independent filter
 
 % x = random_init(nx, ny, Vf);   %initial design for microscale FEM
 x = design1(nx, ny);
 % x = design2(nx, ny);
-E = [1 0.01];
+E = [1 0.1];
 syms xa ya;
 alpha = 25e-6;
 beta = 25e-6;
@@ -27,7 +26,7 @@ Ddash = 1/(1-nu^2)*[1   nu  0;
                     nu  1   0;
                     0   0   (1-nu)/2;];
 KE = double(int(int(B'*Ddash*B, xa, -alpha, alpha), ya, -beta, beta));
-b = double(int(int(B, xa, -alpha, alpha), ya, -beta, beta))
+b = double(int(int(B, xa, -alpha, alpha), ya, -beta, beta));
 % b = 1/4*(subs(subs(B, xa, -alpha*gauss), ya, -beta*gauss) + ...
 % subs(subs(B, xa, alpha*gauss), ya, -beta*gauss) + ...
 % subs(subs(B, xa, alpha*gauss), ya, beta*gauss) + ...
@@ -49,8 +48,8 @@ while (prev_C-C)/C>0.05 || v~=Vf
 %   disp(abs((prev_C-C)/C));
   disp(C);
 %   disp(prev_C);
-  s = sensitivity(nx, ny, x, L, H, Ddash, b, u, B, U);
-%   disp(s);
+  s = sensitivity(nx, ny, x, L, H, Ddash, E, u, B, U);
+  disp(s);
   s_filtered = apply_filter(nx, ny, s, rmin);
 %   disp(s_filtered);
   avg_s = 0.5*(s_filtered + prev_s);
@@ -95,21 +94,28 @@ for i=1:nx
 end
 end
 
-function s = sensitivity(nx, ny, x, L, H, D1, b, u, B, U)
-D2 = zeros(3,3);
+function s = sensitivity(nx, ny, x, L, H, D, E, u, B1, U)
+alpha = 1e-2; beta = 1e-2;
 syms xa ya;
+n = 1/(4*alpha*beta)*[(alpha-xa)*(beta+ya) (alpha+xa)*(beta+ya) ...
+                      (alpha+xa)*(beta-ya) (alpha-xa)*(beta-ya)];
+B = sym('b', [3 8]);
+for i=1:4
+  B(1:3,i*2-1:i*2) = [diff(n(i), xa)  0;...
+                      0               diff(n(i), ya);...
+                      diff(n(i), ya)  diff(n(i),xa);...
+                      ];
+end
 s(1:ny, 1:nx) = 1;
 for i=1:nx
   for j=1:ny
-    alpha = 25e-6;
-    beta = 25e-6;
+    alpha = 25e-6;    beta = 25e-6;
     n1 = (ny+1)*(i-1)+j;
     n2 = (ny+1)*i+j;
     dof = [2*n1-1; 2*n1; 2*n2-1; 2*n2; 2*n2+1; 2*n2+2; 2*n1+1; 2*n1+2];
-    t2 = double(int(int((eye(3)-B*u(dof,:))'*(D1-D2)*(eye(3)-B*u(dof,:)),...
+    t2 = (E(1)-E(2))*double(int(int((eye(3)-B1*u(dof,:))'*D*(eye(3)-B1*u(dof,:)),...
                           xa, -alpha, alpha), ya, -beta, beta));
-    alpha = 1e-2;
-    beta = 1e-2;
+    alpha = 1e-2;    beta = 1e-2;
     t1 = double(int(int(B'*t2*B, xa, -alpha, alpha), ya, -beta, beta));
     temp = 0;
     for l=1:L
@@ -140,7 +146,16 @@ F = sparse(2*(L+1)*(H+1), 1);
 alpha = 1e-2;
 beta = 1e-2;
 syms xa ya;
-% disp(Dh);
+n = 1/(4*alpha*beta)*[(alpha-xa)*(beta+ya) (alpha+xa)*(beta+ya) ...
+                      (alpha+xa)*(beta-ya) (alpha-xa)*(beta-ya)];
+B = sym('b', [3 8]);
+for i=1:4
+  B(1:3,i*2-1:i*2) = [diff(n(i), xa)  0;...
+                      0               diff(n(i), ya);...
+                      diff(n(i), ya)  diff(n(i),xa);...
+                      ];
+end
+% disp(B);
 %KE is calculated every iteration as Dh is changes every iteration
 KE = double(int(int(B'*Dh*B, xa, -alpha, alpha), ya, -beta, beta));
 % disp(KE);
@@ -198,16 +213,17 @@ for i=1:nx
     %once and for different element it is multiplied by the relative
     %density of element
     k(dof, dof) = k(dof, dof) + (x(j, i)*E(1)+(1-x(j, i))*E(2))*ke;
+%     det(k(dof,dof))
     %load is assembly of elemental loads corresponding to the eqn (5) in
     %the paper. le (elemental load) is calculated once and multiplied by
     %relative density for every element.
     load(dof, :) = load(dof, :) + (x(j, i)*E(1)+(1-x(j, i))*E(2))*le;
   end
 end
-disp(ke);
-disp(det(k));
+% disp(det(ke));
+% disp(det(k));
 u = k\load;
-disp(u);
+% disp(u);
 end
 %% Initial designs for the micro FEM
 function x = random_init(nx, ny, Vf)
--- code/xhuang13/temp.m
+++ code/xhuang13/temp.m
@@ -103,69 +103,103 @@
 % errorInt=abs(actualIntegVal-integralTot) %absolute error
 % end
 
-function temp()
-a = 25e-6; b = 25e-6;
-% n = 1/(4*alpha*beta)*[(alpha-xa)*(beta+ya) (alpha+xa)*(beta+ya) ...
-%                       (alpha+xa)*(beta-ya) (alpha-xa)*(beta-ya)];
-n = {@(x,y)(a-x)*(b+y)/(4*a*b), @(x,y)(a+x)*(b+y)/(4*a*b), @(x,y)(a+x)*(b-y)/(4*a*b), @(x,y)(a-x)*(b-y)/(4*a*b)};
-B = @(x,y)[ - b - y,       0, b + y,     0,   b - y,       0, y - b,     0;
-              0,   a - x,     0, a + x,       0, - a - x,     0, x - a;
-              a - x, - b - y, a + x, b + y, - a - x,   b - y, x - a, y - b]/(4*a*b);
-nu=0.3;
-D = 1/(1-nu^2)*[1   nu  0;
-                nu  1   0;
-                0   0   (1-nu)/2;];
-K = @(x,y) B(x,y)'*D*B(x,y);
-vertices = [-a  b;
-             a  b;
-             a -b;
-            -a -b];
-b = integQuad(B, vertices)
-k = integQuad(K, vertices)
-end
+a =[0.4945   -0.1786   -0.3022    0.0137   -0.2472    0.1786    0.0549   -0.0137;
+   -0.1786    0.4945   -0.0137    0.0549    0.1786   -0.2472    0.0137   -0.3022;
+   -0.3022   -0.0137    0.4945    0.1786    0.0549    0.0137   -0.2472   -0.1786;
+    0.0137    0.0549    0.1786    0.4945   -0.0137   -0.3022   -0.1786   -0.2472;
+   -0.2472    0.1786    0.0549   -0.0137    0.4945   -0.1786   -0.3022    0.0137;
+    0.1786   -0.2472    0.0137   -0.3022   -0.1786    0.4945   -0.0137    0.0549;
+    0.0549    0.0137   -0.2472   -0.1786   -0.3022   -0.0137    0.4945    0.1786;
+   -0.0137   -0.3022   -0.1786   -0.2472    0.0137    0.0549    0.1786    0.4945];
+   
+b =[0.4945   -0.1786   -0.3022    0.0137   -0.2473    0.1786    0.0549   -0.0137;
+   -0.1786    0.4945   -0.0137    0.0549    0.1786   -0.2473    0.0137   -0.3022;
+   -0.3022   -0.0137    0.4945    0.1786    0.0549    0.0137   -0.2473   -0.1786;
+    0.0137    0.0549    0.1786    0.4945   -0.0137   -0.3022   -0.1786   -0.2473;
+   -0.2473    0.1786    0.0549   -0.0137    0.4945   -0.1786   -0.3022    0.0137;
+    0.1786   -0.2473    0.0137   -0.3022   -0.1786    0.4945   -0.0137    0.0549;
+    0.0549    0.0137   -0.2473   -0.1786   -0.3022   -0.0137    0.4945    0.1786;
+   -0.0137   -0.3022   -0.1786   -0.2473    0.0137    0.0549    0.1786    0.4945];
 
-function valInteg = integQuad(F,vertices)
-    w = [1 1 1 1];
-    ptGaussRef =[-0.5774   -0.5774;
-                 -0.5774    0.5774;
-                  0.5774   -0.5774;
-                  0.5774    0.5774];
-    % Shape functions
-    Psi1=@(x,y)(1-x).*(1-y)/4;
-    Psi2=@(x,y)(1+x).*(1-y)/4;
-    Psi3=@(x,y)(1+x).*(1+y)/4;
-    Psi4=@(x,y)(1-x).*(1+y)/4;
-    % Shape function derivatives
-    dPsi11=@(x,y) -(1-y)/4;
-    dPsi21=@(x,y) (1-y)/4;
-    dPsi31=@(x,y) (1+y)/4;
-    dPsi41=@(x,y) -(1+y)/4;
-    dPsi12=@(x,y) -(1-x)/4;
-    dPsi22=@(x,y) -(1+x)/4;
-    dPsi32=@(x,y) (1+x)/4;
-    dPsi42=@(x,y) (1-x)/4;
-    % Gradient matrix
-    Jacb =@(x,y) [dPsi11(x,y), dPsi21(x,y),dPsi31(x,y),dPsi41(x,y);...
-                  dPsi12(x,y), dPsi22(x,y),dPsi32(x,y),dPsi42(x,y)];
-    % evaluate Shape functions on Gaussian reference points
-    xi = ptGaussRef(:,1);
-    eta = ptGaussRef(:,2);
-    evalPsi1 = Psi1(xi,eta);
-    evalPsi2 = Psi2(xi,eta);
-    evalPsi3 = Psi3(xi,eta);
-    evalPsi4 = Psi4(xi,eta);
-    % from the change of variables function
-    ptGaussDomain = [evalPsi1,evalPsi2,evalPsi3,evalPsi4]*vertices;
-    % evaluate Jacobian contribution for each point
-    for i=1:size(xi,1)
-        evalDetJacb(i) = abs(det(Jacb(xi(i),eta(i))*vertices));
-    end
-    %evaluate the function on the domain points
-    %evalF=F(ptGaussDomain(:,1),ptGaussDomain(:,2));
-    % Finally, apply Gauss formula
-    suma=zeros(size(F(vertices(1,1), vertices(1,2))));
-    for i=1:size(ptGaussDomain,1)
-        suma=suma+w(i)*F(ptGaussDomain(i,1),ptGaussDomain(i,2))*evalDetJacb(i);
-    end
-    valInteg = suma;
-end
+E = 1.; 
+nu = 0.3;
+k=[ 1/2-nu/6   1/8+nu/8 -1/4-nu/12 -1/8+3*nu/8 ... 
+   -1/4+nu/12 -1/8-nu/8  nu/6       1/8-3*nu/8];
+KE = E/(1-nu^2)*[ k(1) k(2) k(3) k(4) k(5) k(6) k(7) k(8)
+                  k(2) k(1) k(8) k(7) k(6) k(5) k(4) k(3)
+                  k(3) k(8) k(1) k(6) k(7) k(4) k(5) k(2)
+                  k(4) k(7) k(6) k(1) k(8) k(3) k(2) k(5)
+                  k(5) k(6) k(7) k(8) k(1) k(2) k(3) k(4)
+                  k(6) k(5) k(4) k(3) k(2) k(1) k(8) k(7)
+                  k(7) k(4) k(5) k(2) k(3) k(8) k(1) k(6)
+                  k(8) k(3) k(2) k(5) k(4) k(7) k(6) k(1)];
+disp(det(a))
+disp(det(b))
+disp(det(KE))
+
+% function temp()
+% a = 25e-6; b = 25e-6;
+% % n = 1/(4*alpha*beta)*[(alpha-xa)*(beta+ya) (alpha+xa)*(beta+ya) ...
+% %                       (alpha+xa)*(beta-ya) (alpha-xa)*(beta-ya)];
+% n = {@(x,y)(a-x)*(b+y)/(4*a*b), @(x,y)(a+x)*(b+y)/(4*a*b), @(x,y)(a+x)*(b-y)/(4*a*b), @(x,y)(a-x)*(b-y)/(4*a*b)};
+% B = @(x,y)[ - b - y,       0, b + y,     0,   b - y,       0, y - b,     0;
+%               0,   a - x,     0, a + x,       0, - a - x,     0, x - a;
+%               a - x, - b - y, a + x, b + y, - a - x,   b - y, x - a, y - b]/(4*a*b);
+% nu=0.3;
+% D = 1/(1-nu^2)*[1   nu  0;
+%                 nu  1   0;
+%                 0   0   (1-nu)/2;];
+% K = @(x,y) B(x,y)'*D*B(x,y);
+% vertices = [-a  b;
+%              a  b;
+%              a -b;
+%             -a -b];
+% b = integQuad(B, vertices)
+% k = integQuad(K, vertices)
+% end
+% 
+% function valInteg = integQuad(F,vertices)
+%     w = [1 1 1 1];
+%     ptGaussRef =[-0.5774   -0.5774;
+%                  -0.5774    0.5774;
+%                   0.5774   -0.5774;
+%                   0.5774    0.5774];
+%     % Shape functions
+%     Psi1=@(x,y)(1-x).*(1-y)/4;
+%     Psi2=@(x,y)(1+x).*(1-y)/4;
+%     Psi3=@(x,y)(1+x).*(1+y)/4;
+%     Psi4=@(x,y)(1-x).*(1+y)/4;
+%     % Shape function derivatives
+%     dPsi11=@(x,y) -(1-y)/4;
+%     dPsi21=@(x,y) (1-y)/4;
+%     dPsi31=@(x,y) (1+y)/4;
+%     dPsi41=@(x,y) -(1+y)/4;
+%     dPsi12=@(x,y) -(1-x)/4;
+%     dPsi22=@(x,y) -(1+x)/4;
+%     dPsi32=@(x,y) (1+x)/4;
+%     dPsi42=@(x,y) (1-x)/4;
+%     % Gradient matrix
+%     Jacb =@(x,y) [dPsi11(x,y), dPsi21(x,y),dPsi31(x,y),dPsi41(x,y);...
+%                   dPsi12(x,y), dPsi22(x,y),dPsi32(x,y),dPsi42(x,y)];
+%     % evaluate Shape functions on Gaussian reference points
+%     xi = ptGaussRef(:,1);
+%     eta = ptGaussRef(:,2);
+%     evalPsi1 = Psi1(xi,eta);
+%     evalPsi2 = Psi2(xi,eta);
+%     evalPsi3 = Psi3(xi,eta);
+%     evalPsi4 = Psi4(xi,eta);
+%     % from the change of variables function
+%     ptGaussDomain = [evalPsi1,evalPsi2,evalPsi3,evalPsi4]*vertices;
+%     % evaluate Jacobian contribution for each point
+%     for i=1:size(xi,1)
+%         evalDetJacb(i) = abs(det(Jacb(xi(i),eta(i))*vertices));
+%     end
+%     %evaluate the function on the domain points
+%     %evalF=F(ptGaussDomain(:,1),ptGaussDomain(:,2));
+%     % Finally, apply Gauss formula
+%     suma=zeros(size(F(vertices(1,1), vertices(1,2))));
+%     for i=1:size(ptGaussDomain,1)
+%         suma=suma+w(i)*F(ptGaussDomain(i,1),ptGaussDomain(i,2))*evalDetJacb(i);
+%     end
+%     valInteg = suma;
+% end
